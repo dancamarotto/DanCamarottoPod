1dcf94528564075791700dfc8b4b49035dfb19cb
Danilo Camarotto <danilocamarotto@wizsolucoes.com.br>
2018-04-06 11:42:14 -0300

    Initial commit

---
+++ DanCamarottoPod/BooleanInverter.swift
@@ -0,0 +1,17 @@
+//
+//  BooleanInverter.swift
+//  DanCamarottoPod
+//
+//  Created by Danilo Camarotto on 06/04/18.
+//  Copyright © 2018 Danilo Camarotto. All rights reserved.
+//
+
+import Foundation
+
+public class BooleanInverter {
+    
+    public static func invert(_ value: Bool) -> Bool {
+        return !value
+    }
+    
+}
---
+++ DanCamarottoPod/DanCamarottoPod.h
@@ -0,0 +1,19 @@
+//
+//  DanCamarottoPod.h
+//  DanCamarottoPod
+//
+//  Created by Danilo Camarotto on 06/04/18.
+//  Copyright © 2018 Danilo Camarotto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DanCamarottoPod.
+FOUNDATION_EXPORT double DanCamarottoPodVersionNumber;
+
+//! Project version string for DanCamarottoPod.
+FOUNDATION_EXPORT const unsigned char DanCamarottoPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DanCamarottoPod/PublicHeader.h>
+
+
